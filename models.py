9bae0f3d544048497dec4fa7e32d13db51bb66aa
Sriram V <aimrrs404@gmail.com>
2025-09-07 08:58:57 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, St
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import JSONB
-from .database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
